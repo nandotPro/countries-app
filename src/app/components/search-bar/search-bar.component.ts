ceee173aa43eb656b387137e9901b5780f176135
NaN <ipfilus@gmail.com>
2025-03-29 17:57:10 -0300

    feat: add search bar component

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+  @Output() search = new EventEmitter<string>();
+  
+  searchTerm: string = '';
+  
+  onSearch(): void {
+    this.search.emit(this.searchTerm);
+  }
+}
